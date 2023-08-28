adec76ba6e7130afa7716a522ce231ace08ebb7e
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2023-08-28 05:50:27 +0530

    Created Sign In form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="bgImage"
+          srcset=""
+        />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+        <p className="py-4">New to Netflix? Sign Up Now</p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
