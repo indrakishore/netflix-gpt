fea3a982f27a58167dbdbca1a1245b135d7fbe92
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2023-08-29 20:28:51 +0530

    Implemented custom hooks useNowPlayingMovies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,8 @@
-import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const jsonData = await data.json();
-    console.log(jsonData.results);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
---
+++ src/utils/moviesSlice.js
@@ -0,0 +1,16 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const moviesSlice = createSlice({
+  name: "movies",
+  initialState: {
+    addNowPlayingMovies: null,
+  },
+  reducers: {
+    addNowPlayingMovies: (state, action) => {
+      state.addNowPlayingMovies = action.payload;
+    },
+  },
+});
+
+export const { addNowPlayingMovies } = moviesSlice.actions;
+export default moviesSlice.reducer;
