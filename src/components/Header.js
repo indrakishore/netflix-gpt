150daddfab4e193c2080cc745d25008490abe26d
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2023-09-04 03:07:43 +0530

    Build GptSearch Toggle Functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -48,11 +49,22 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    //Toggle GptSearch
+    dispatch(toggleGptSearchView());
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
+          <button
+            className="py-2 m-2 px-4 mx-4 my-2 text-white bg-purple-800 rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            GPT Search
+          </button>
           <img className="w-12 h-12" src={user?.photoURL} alt="user-avatar" />
           <button onClick={handleSignOut} className="font-bold text-white">
             Sign Out
