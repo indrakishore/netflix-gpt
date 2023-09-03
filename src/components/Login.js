b09ddf9f68993db260a9dbdaf40f02371e16fb2e
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2023-09-04 03:31:00 +0530

    Build SearchBar

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ import { auth } from "../utils/firebase";
 import { updateProfile } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import { USER_AVATAR } from "../utils/constants";
+import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -88,10 +88,7 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
-          alt="bgImage"
-        />
+        <img src={BG_URL} alt="bgImage" />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
