25d75093a0e10b317172eda10718e4de07b85e0f
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2023-09-05 00:17:45 +0530

    Made NetflixGPT responsive

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,8 +5,8 @@ const MovieList = ({ title, movies }) => {
   return (
     movies && (
       <div className="px-6">
-        <h1 className="text-3xl py-4 text-white">{title}</h1>
-        <div className="flex ">
+        <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
+        <div className="flex overflow-x-scroll scroll-m-0 scroll-p-0">
           <div className="flex">
             {movies?.map((movie) => (
               <MovieCard key={movie.id} posterPath={movie.poster_path} />
